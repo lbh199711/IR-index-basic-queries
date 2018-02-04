47e191c61284807444ee12cdc244e640e9ea0674
lbh199711 <31713252+lbh199711@users.noreply.github.com>
2018-02-04 15:24:01 -0700

    added print index

---
+++ print_index.py
@@ -0,0 +1,59 @@
+import sys
+import sqlite3
+def main():
+    #connect to database
+    if len(sys.argv) > 2:
+        print ("invalid arguement")
+        return 
+    elif len(sys.argv)== 1:
+        index_dir = "."
+    else:
+        index_dir = str(sys.argv[1])
+    
+    
+    conn = sqlite3.connect(index_dir+'/table.db')
+    c = conn.cursor()
+    try:
+        c.execute("SELECT * from searchIndex order by word,movie_id")
+    except sqlite3.OperationalError:
+        print("Table don't exist, run create_index.py first")
+        return
+    
+    row = c.fetchone()
+    old = ""
+    word = row[0]
+    z = 1
+    while(1):
+        z += 1
+        movie_id = str(row[1])
+        position = row[2].replace(",","").split(" ")
+
+        
+
+        if word != old:
+            print (word,end="\t")
+        print(movie_id,end=":")
+        for i in range(len(position)):
+            print(position[i],end="")
+            if i != len(position)-1:
+                print(",",end="")#if not at the end print ','
+            
+        
+
+        #check next one
+        row = c.fetchone()
+        old = word
+        #if next word is different then don't print ;
+        if row is not None:
+            word = row[0]
+        else:
+            break
+        if word == old:
+            print(";",end="")
+        else:
+            print("\n",end="")
+         
+    print("\n",end="")
+
+
+main()
