3b4791639cb5216f4cfc400b531f6e1aa90f7daa
Leo <lbh199711@gmail.com>
2018-02-09 11:25:14 -0700

    finished

--- boolean_query.py
+++ boolean_query.py
@@ -68,7 +68,7 @@ def not_query(list1,conn):
     x = 0
     y = 0
     outputlist=[]
-    print(list1)
+
     while(1):
 		#now $total includes $list1 we just need to take out $list1
         if (x==len(list1)):#end when list1 end
@@ -84,7 +84,7 @@ def not_query(list1,conn):
         elif list1[x][0]<total[y][0]:#list1[x] should never be smaller than total[y]
             print("something went wrong in not_query x=",x," y=",y)
             return -1
-    print(outputlist)
+
     return outputlist
 
 
@@ -133,6 +133,32 @@ def term_query(list1,list2):
     return outputlist
         
 
+def condition_query(query_list,conn):
+    #get a list of word and check the length then do query
+    output = []
+    if len(query_list)==3:#this is boolean query
+        #this is the word/list to query for,middle one is op
+        for i in [0,2]:
+            if type(query_list[i]) == list:
+                output.append(query_list[i])
+            else: 
+                c = conn.cursor()
+                c.execute("SELECT DISTINCT movie_id FROM searchIndex WHERE word='"+query_list[i]+"';")
+                output.append(c.fetchall())
+        op = query_list[1]
+        output = boolean_query(op,output[0],output[1])
+    elif len(query_list)==1:#this is to just find the term
+        if type(query_list[0])==list:
+            output=query_list[0]
+        else:
+            c = conn.cursor()
+            c.execute("SELECT DISTINCT movie_id FROM searchIndex WHERE word='"+query_list[0]+"';")
+            output=c.fetchall()
+    else:#error
+        return -1
+    return output
+         
+
 	
 def main():
     #connect to database
@@ -159,14 +185,19 @@ def main():
     query = query.replace(")"," ) ")
     query = query.lower().split()
     for i in range(len(query)):
-        if query[i] != " ( " and query[i] != " ) ":
+        if query[i][0]=='"' or query[i][0]=="'" or query[i][-1]=='"' or query[i][-1]=="'":
+            continue 
+        if query[i] != "(" and query[i] != ")":
             query[i] = clean_word(query[i])
-    
+
     #get rid of the term querys first
     term_temp = []#list of word in a term query
     term_inside = False
+
     for i in range(len(query)):
         word = query[i]
+        if word == '' or word == ' ' or word is None:#if word is empty
+            continue
         if term_inside:#if it is inside a term query
             if word[-1]=="'"or word[-1]=='"':#see if it is the end
                 term_temp.append(word[:-1])#add to temp list with
@@ -193,23 +224,59 @@ def main():
                 query[i]=None
             else: 
                 continue#else just pass
-        
+    
     #now while loop til done
     i = 0 #<- pointer
-    in_sub_query=False                  
+    in_sub_query=False  
+    if query[0] != '(' or query[-1]!=')':
+        query.insert(0,'(')
+        query.append(')')
     while(1):
-        if query[i]==None:#clear out the filler objects
+
+        if query[i] == -1:
+            "invalid query"
+            break
+
+        if query[i]==None or query[i]==' ' or query[i] == '':#clear out the filler objects
             query.pop(i)
+            i-= 1
         elif query[i]=="(":#trying to find the most inner (
             sub_query= []#clear out the list 
             in_sub_query = True
         elif query[i]==")":#end of a subquery and do stuff
             in_sub_query= False
+            query[i] = condition_query(sub_query,conn)
+
+            for j in range(len(sub_query)+1):
+                i -= 1#go back
+                query[i] = None#remove from the query
+            #go one back and check if it is NOT
+            if query[i-1] == 'not' or query[i-1] == 'NOT':
+                query[i-1] = None
+                query[i+len(sub_query)+1] = not_query(query[i+len(sub_query)+1],conn)#do not query 
+            sub_query=[]#clear out the list
+            i = 0 #go from the beginning
+            continue
+        elif in_sub_query:#if in subquery just add it 
+            sub_query.append(query[i])       
             
         i += 1#next
-    
+
+        #check end condition
+        if len(query)==1:
+            if query[0] == -1:
+                print ("invalid input")
+            break
+        elif i == len(query):
+            i = 0#loop around
+            if in_sub_query:#if it go to the end but didn't finish sub_query
+                print("invalid input")
+                return -1
+        
     conn.close()
     
-    print (sys.argv)
-    print (query)
+    for i in query[0]:
+        print (i[0])
+
+ 
 main()
